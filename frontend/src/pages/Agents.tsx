db74f1834a92b1beb181ef059727b956c21b6700
Priyansu <priyansumohapatra11@gmail.com>
2026-08-23 09:55:34 +0530

    OPL-service: removed files

--- frontend/src/pages/Agents.tsx
+++
@@ -1,7 +0,0 @@
-import { Typography } from "@mui/material";
-
-function Agents() {
-  return <Typography variant="h4">Agents Workspace</Typography>;
-}
-
-export default Agents;
--- frontend/src/pages/DatasetTables.tsx
+++
@@ -1,5 +0,0 @@
-// file:///c:/Users/ansuj/OpenLens/frontend/src/pages/DatasetTables.tsx
-
-export default function DatasetTables() {
-  return null;
-}
